828bcf82d084002e839c57fe96c6864d827335a9
Vu Anh Hao <hi@haova.me>
2023-03-29 11:18:41 +0700

    Added Secure super class from shared

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,8 @@
-import { mergeDeepLeft } from "ramda";
+import { mergeDeepLeft } from 'ramda';
+import { Secure } from '@rugo-vn/shared';
 
 export const run = async function ({ id, entry, locals, spaceId }) {
   const getFn = async function (nextFilePath) {
-
     const file = await this.call('storage.get', { ...id, path: nextFilePath });
 
     return file.data.toText();
@@ -10,18 +10,26 @@ export const run = async function ({ id, entry, locals, spaceId }) {
 
   const tableFn = function (tableName) {
     const that = this;
-    return new Proxy({}, {
-      get(_, prop) {
-        return args => that.call(`db.${prop}`, mergeDeepLeft({ spaceId, tableName }, args));
-      },
-    });
+    return new Proxy(
+      {},
+      {
+        get(_, prop) {
+          return (args) =>
+            that.call(
+              `db.${prop}`,
+              mergeDeepLeft({ spaceId, tableName }, args)
+            );
+        },
+      }
+    );
   };
 
   return await this.fx.run(entry, {
     get: getFn.bind(this),
     locals: {
       ...locals,
-      table: tableFn.bind(this)
-    }
+      table: tableFn.bind(this),
+      Secure,
+    },
   });
 };
